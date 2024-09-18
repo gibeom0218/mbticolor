096653aae19675ade22cb4f54b1f53c544df03f0
beom <gb3187@naver.com>
2024-09-19 06:09:29 +0900

    feat : 라우터 설정

---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AssignColorPage from './pages/AssignColorPage';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/AssignColor" element={<AssignColorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/pages/AssignColorPage.tsx
@@ -0,0 +1,5 @@
+const AssignColorPage = () => {
+  return <div>안녕2</div>;
+};
+
+export default AssignColorPage;
