a4181f1a86bb6a842ca0ade46b3b5e88b15ee3bc
beom <gb3187@naver.com>
2024-09-20 10:17:55 +0900

    refactor : 디자인 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import MbtiColorInfo from '../components/MbtiColorInfo';
 
 const HomePage = () => {
   return (
-    <div className="flex pt-[24px] px-[16px] flex-col justify-end items-start gap-[16px] bg-white tablet:px-[230px] tablet:flex-row tablet:gap-[60px] tablet:mt-[80px]">
+    <div className="flex h-screen pt-[24px] px-[16px] pb-[24px] flex-col items-start gap-[16px] bg-white tablet:px-[230px] tablet:flex-row tablet:gap-[60px] tablet:pt-[55px]">
       <Header />
       <MbtiColorInfo />
     </div>
