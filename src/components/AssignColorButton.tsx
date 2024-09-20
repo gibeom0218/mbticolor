d12f3792719f5ac6118a65f8823b64e39d4f0772
beom <gb3187@naver.com>
2024-09-20 10:14:06 +0900

    feat : 컬러 등록 버튼 디자인

---
+++ src/components/AssignColorButton.tsx
@@ -0,0 +1,11 @@
+const AssignColorButton = () => {
+  return (
+    <button
+      type="button"
+      className="flex px-[22px] py-[22px] justify-center items-center rounded-[16px] bg-[#464E5E] text-[24px] font-bold text-white"
+    >
+      컬러 등록
+    </button>
+  );
+};
+export default AssignColorButton;
--- src/pages/AssignColorPage.tsx
+++ src/pages/AssignColorPage.tsx
@@ -1,13 +1,15 @@
+import AssignColorButton from '../components/AssignColorButton';
 import AssignColorHeader from '../components/AssignColorHeader';
 import ColorSelectContent from '../components/ColorSelectContent';
 import MbtiSelectContent from '../components/MbtiSelectContent';
 
 const AssignColorPage = () => {
   return (
-    <div className="flex flex-col px-[24px] py-[42px] tablet:px-[288px] gap-[50px]">
+    <div className="flex flex-col h-screen px-[24px] py-[42px] tablet:px-[288px] gap-[50px]">
       <AssignColorHeader />
       <MbtiSelectContent />
       <ColorSelectContent />
+      <AssignColorButton />
     </div>
   );
 };
