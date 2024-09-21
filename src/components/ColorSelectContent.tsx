0d0e3efdacf19e41b90bf68424bb19049df836e2
beom <gb3187@naver.com>
2024-09-21 18:41:54 +0900

    feat : 색 선택 모달창 구현

--- src/components/ColorSelectContent.tsx
+++ src/components/ColorSelectContent.tsx
@@ -1,16 +1,30 @@
+import React, { useState } from 'react';
+import SelectColorModal from './modal/SelectColorModal';
+
 const ColorSelectContent = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
     <div className="flex flex-col gap-[15px]">
       <div className="flex gap-[3px] items-center">
         <p className="text-[#464E5E] text-[24px] font-medium">컬러</p>
-        <div className="p-[8px] inline-flex rounded-[8px] bg-[#F5F7FB]">
+        <button type="button" className="p-[8px] inline-flex rounded-[8px] bg-[#F5F7FB]" onClick={handleOpenModal}>
           <img src="/icons/repeat.svg" alt="랜덤 아이콘" />
-        </div>
+        </button>
       </div>
       <div className="inline-flex p-[25px] justify-between items-center rounded-[16px] border-[2px] border-solid border-[#E7EBF2]">
         <p className="text-[#8892A6] text-[24px] font-medium">#8892A6</p>
         <div className="w-[40px] h-[40px] rounded-[8px] border-[3px] border-solid border-[#E8E8E8] bg-[#9441FF]" />
       </div>
+      {isOpenModal && <SelectColorModal closeModal={handleCloseModal} />}
     </div>
   );
 };
