c42c952cade2c3ac6010620874fdce97f2586bc2
beom <gb3187@naver.com>
2024-09-22 09:38:00 +0900

    feat : 리액트 쿼리 다운로드 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Reset } from 'styled-reset';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Router from './Router';
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ToastContainer
         position="top-center"
         autoClose={3000} // 3초 후 자동으로 닫힘
@@ -19,7 +22,8 @@ const App = () => {
       />
       <Reset />
       <Router />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
