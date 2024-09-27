32375de66e4ea66e93bc6df861d67f0868d7e2ac
beom <gb3187@naver.com>
2024-09-27 12:30:50 +0900

    feat : useQuery을 사용하여 수정 페이지인 경우에만 해당 id의 데이터 가져오기

--- src/pages/AssignColorPage.tsx
+++ src/pages/AssignColorPage.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom'; // useParams 사용
+import getColorSurveyById from '../api/getColorSurveyById';
 import AssignColorButton from '../components/AssignColorButton';
 import AssignColorHeader from '../components/AssignColorHeader';
 import ColorSelectContent from '../components/ColorSelectContent';
@@ -9,11 +10,12 @@ import { FunType } from '../types/funType';
 const AssignColorPage = ({ type }: FunType) => {
   const { id } = useParams<{ id: string }>();
 
-  useEffect(() => {
-    if (type === 'Modify') {
-      console.log(id);
-    }
-  }, []);
+  // React Query로 서버에서 데이터 가져오기
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['colorSurvey', id],
+    queryFn: () => getColorSurveyById(Number(id)),
+    enabled: !!id && type === 'Modify',
+  });
 
   return (
     <div className="flex flex-col h-screen px-[24px] py-[42px] tablet:px-[288px] gap-[50px]">
